c0c4eaf557d4684d8fb9bdf8a635de44961c3af4
RogersonFelipe <rogersonfilipi@hotmail.com>
2026-02-09 00:35:39 -0300

    Atualizando

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -4,14 +4,12 @@ import { Project } from 'src/projects/entities/project.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST || process.env.PGHOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT || process.env.PGPORT || '5432', 10),
-  username: process.env.DATABASE_USER || process.env.PGUSER || 'postgres',
-  password:
-    process.env.DATABASE_PASSWORD || process.env.PGPASSWORD || 'postgres',
-  database:
-    process.env.DATABASE_NAME || process.env.PGDATABASE || 'heroforce_db',
+  url: process.env.DATABASE_URL,
   entities: [User, Project],
   synchronize: true,
   logging: process.env.NODE_ENV === 'development',
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 };
