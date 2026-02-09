aed5fc080e8e178b38efffaf7a47e2eac62e114d
RogersonFelipe <rogersonfilipi@hotmail.com>
2026-02-09 00:25:30 -0300

    Ajustando_Backend_Railway

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -4,11 +4,13 @@ import { Project } from 'src/projects/entities/project.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'postgres',
-  port: parseInt(process.env.DATABASE_PORT || '5432'),
-  username: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'postgres',
-  database: process.env.DATABASE_NAME || 'heroforce_db',
+  host: process.env.DATABASE_HOST || process.env.PGHOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT || process.env.PGPORT || '5432', 10),
+  username: process.env.DATABASE_USER || process.env.PGUSER || 'postgres',
+  password:
+    process.env.DATABASE_PASSWORD || process.env.PGPASSWORD || 'postgres',
+  database:
+    process.env.DATABASE_NAME || process.env.PGDATABASE || 'heroforce_db',
   entities: [User, Project],
   synchronize: true,
   logging: process.env.NODE_ENV === 'development',
