f27e20fe37d935f1e0fce4ceb849c7fb170ec19f
RogersonFelipe <rogersonfilipi@hotmail.com>
2026-02-10 18:21:14 -0300

    Ajuste_axios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -11,7 +11,7 @@ import {
 } from '../types';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
