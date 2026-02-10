c2ed93bc60a2ea59ecaedb88605c6edc550a4449
RogersonFelipe <rogersonfilipi@hotmail.com>
2026-02-10 17:15:02 -0300

    Ajuste_Deploy_Front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 5173,
-    strictPort: false,
   },
   preview: {
     host: "0.0.0.0",
     port: parseInt(process.env.PORT || "4173"),
-    strictPort: false,
-    cors: true,
+    allowedHosts: ["all"],
   },
 });
